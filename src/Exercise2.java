692841b2a9ad1c47a9d34b0742434bdacf69f814
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 14:19:01 +0100

    Create Exercise2.java

---
+++ src/Exercise2.java
@@ -0,0 +1,18 @@
+/**
+ * Week 2, Exercise 2.
+ * @author INSERT YOUR NAME HERE
+ */
+public class Exercise2 {
+
+	public static void main(String[] args) {
+		// Declare variables
+		
+		
+		// Assign variables
+		
+		
+		// Print them out
+		
+	}
+
+}
