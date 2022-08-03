09a0dbea53364ccb5560dd67ed9d97b55205ca2a
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 14:19:09 +0100

    Create Exercise4.java

---
+++ src/Exercise4.java
@@ -0,0 +1,13 @@
+/**
+ * Week 2, Exercise 4.
+ * @author Michael Lones
+ */
+public class Exercise4 {
+
+	public static void main(String[] args) {
+		/* First, copy across the code in the main method
+		   from your solution to Exercise 1. */
+
+	}
+
+}
