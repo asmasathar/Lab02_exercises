46f71c1e00b7432c8afc3a76884f8cdb0c0c4933
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 14:19:14 +0100

    Create Exercise6.java

---
+++ src/Exercise6.java
@@ -0,0 +1,13 @@
+/**
+ * Week 2, Exercise 6.
+ * @author INSERT YOUR NAME HERE
+ */
+public class Exercise6 {
+
+	public static void main(String[] args) {
+		/* First, copy across the code from your
+		   solution to Exercise 4. */
+
+	}
+
+}
