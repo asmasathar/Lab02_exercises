7283f0c5079bddbc5fc824353b36a120c8517257
Batatia, Hadj <69246983+hbatatia@users.noreply.github.com>
2022-08-03 14:18:58 +0100

    Create Exercise1.java
    initial commit

---
+++ src/Exercise1.java
@@ -0,0 +1,15 @@
+/**
+ * Week 2, Exercise 1.
+ * @author INSERT YOUR NAME HERE
+ */
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// Declare and assign variables just below here
+
+		
+		// Then add code to print them out below here
+		
+	}
+
+}
